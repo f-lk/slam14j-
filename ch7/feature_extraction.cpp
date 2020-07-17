d87207734cb1bb673bc41e1bd2d84bb00fafff5e
深渊为陵 <53933714+f-lk@users.noreply.github.com>
2020-07-17 21:22:54 +0800

    Add files via upload

---
+++ ch7/feature_extraction.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace std;
+using namespace cv;
+
+int main ( int argc, char** argv )
+{
+    if ( argc != 3 )
+    {
+        cout<<"usage: feature_extraction img1 img2"<<endl;
+        return 1;
+    }
+    //-- 读取图像
+    Mat img_1 = imread ( argv[1], IMREAD_REDUCED_COLOR_2 );
+    Mat img_2 = imread ( argv[2], IMREAD_REDUCED_COLOR_2 );
+//cout <<img_1<<endl;
+    //-- 初始化
+    std::vector<KeyPoint> keypoints_1, keypoints_2;
+    Mat descriptors_1, descriptors_2;
+    Ptr<FeatureDetector> detector = ORB::create();
+    Ptr<DescriptorExtractor> descriptor = ORB::create();
+    // Ptr<FeatureDetector> detector = FeatureDetector::create(detector_name);
+    // Ptr<DescriptorExtractor> descriptor = DescriptorExtractor::create(descriptor_name);
+    Ptr<DescriptorMatcher> matcher  = DescriptorMatcher::create ( "BruteForce-Hamming" );
+
+    //-- 第一步:检测 Oriented FAST 角点位置
+    detector->detect ( img_1,keypoints_1 );
+    detector->detect ( img_2,keypoints_2 );
+ //   drawKeypoints( img_1, keypoints_1, keypoints_1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+    //waitKey(0);
+
+    //-- 第二步:根据角点位置计算 BRIEF 描述子
+    descriptor->compute ( img_1, keypoints_1, descriptors_1 );
+    descriptor->compute ( img_2, keypoints_2, descriptors_2 );
+
+
+    Mat outimg1;
+    drawKeypoints( img_1, keypoints_1, outimg1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+    Mat dst_1;
+    resize(outimg1, dst_1, Size(),0.7,0.7);//我长宽都变为原来的0.5倍
+
+    imshow("尺寸调整之后,ORB特征点", dst_1);
+    //imshow("ORB特征点",outimg1);
+    waitKey(0);
+
+    //-- 第三步:对两幅图像中的BRIEF描述子进行匹配，使用 Hamming 距离
+    vector<DMatch> matches;
+    //BFMatcher matcher ( NORM_HAMMING );
+    matcher->match ( descriptors_1, descriptors_2, matches );
+
+    //-- 第四步:匹配点对筛选
+    double min_dist=10000, max_dist=0;
+
+    //找出所有匹配之间的最小距离和最大距离, 即是最相似的和最不相似的两组点之间的距离
+    for ( int i = 0; i < descriptors_1.rows; i++ )
+    {
+        double dist = matches[i].distance;
+        if ( dist < min_dist ) min_dist = dist;
+        if ( dist > max_dist ) max_dist = dist;
+    }
+    
+    // 仅供娱乐的写法
+    min_dist = min_element( matches.begin(), matches.end(), [](const DMatch& m1, const DMatch& m2) {return m1.distance<m2.distance;} )->distance;
+    max_dist = max_element( matches.begin(), matches.end(), [](const DMatch& m1, const DMatch& m2) {return m1.distance<m2.distance;} )->distance;
+
+    printf ( "-- Max dist : %f \n", max_dist );
+    printf ( "-- Min dist : %f \n", min_dist );
+
+    //当描述子之间的距离大于两倍的最小距离时,即认为匹配有误.但有时候最小距离会非常小,设置一个经验值30作为下限.
+    std::vector< DMatch > good_matches;
+    for ( int i = 0; i < descriptors_1.rows; i++ )
+    {
+        if ( matches[i].distance <= max ( 2*min_dist, 30.0 ) )//
+        {
+            good_matches.push_back ( matches[i] );
+        }
+    }
+
+    //-- 第五步:绘制匹配结果
+    Mat img_match;
+    Mat img_goodmatch;
+    drawMatches ( img_1, keypoints_1, img_2, keypoints_2, matches, img_match );
+    drawMatches ( img_1, keypoints_1, img_2, keypoints_2, good_matches, img_goodmatch );
+    Mat dst_2;
+    resize(img_match, dst_2, Size(),0.7,0.7);//长宽都变为原来的0.7倍
+
+    imshow("所有匹配点对", dst_2);
+    Mat dst_3;
+    resize(img_goodmatch, dst_3, Size(),0.7,0.7);//长宽都变为原来的0.7倍
+
+    imshow("尺寸调整之后,ORB特征点", dst_3);
+  //  imshow ( "", img_match );
+  //  imshow ( "优化后匹配点对", img_goodmatch );
+    waitKey(0);
+
+    return 0;
+}
